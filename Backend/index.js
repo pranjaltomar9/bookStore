4cfc1f43f0aa241923e1ad36031a5a7b16e40a69
pranjaltomar9 <tomarpranjal9@gmail.com>
2025-07-28 19:36:06 +0530

    backend initialisation complete

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express()
+dotenv.config();
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`)
+
+        app.on("error", (error) => {
+            console.log("ERRR: ", error);
+            throw error            
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);            
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
